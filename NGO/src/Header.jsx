6f7a04aa70f91081f4b479dce6c19e4a93215599
mohameddamrr <mohmd.amr@hotmail.com>
2024-05-04 19:41:31 +0300

    .

---
+++ NGO/src/Header.jsx
@@ -0,0 +1,6 @@
+function Header(){
+return(
+    <h1>HELLO</h1>
+);
+}
+export default Header
---
+++ NGO/src/Home.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+const Home = () => {
+    return (
+        <hi>in home component</hi>
+      );
+}
+ 
+export default Home;
