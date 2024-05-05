a43d453c785560094e17fa6f59b96434c76ecfa7
mohameddamrr <mohmd.amr@hotmail.com>
2024-05-05 19:36:47 +0300

    App.js

--- NGO/src/App.js
+++ NGO/src/App.js
@@ -2,43 +2,46 @@
 
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Settings from './Settings';
-
+import Organization from './Organization/Organization'
+import location from './Organization/pages/locationpage';
 function App() {
   return (
-    <Router>
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/Settings">
-            <Settings/>
-          </Route>
+    // <Router>
+    //   <div className="App">
+    //     {/* <Navbar/> */}
+    //     <div className="content">
+    //       <Routes>
+    //         <Route exact path="/">
+    //           <Home/>
+    //         </Route>
+    //         <Route path="/Settings">
+    //           <Settings/>
+    //         </Route>
+
+    //         {/* Organization */}
+    //         <Route path="/organization" element={<Organization />}>
+    //         </Route>
 
 
-        </Switch>
-        
+    //       </Routes>
+          
+    //     </div>
 
-
-
-
-
-
-
-        </div>
-
-      </div>
-      </Router>
+    //   </div>
+    // </Router>
       
-
-
-
+    <div>
+    <Routes>
+      <Route path="/organization" element={<Organization />}>
+        <Route path="location" element={<locationpage />} />
+      </Route>
+    </Routes>
+  </div>
+  
 
 
   );
